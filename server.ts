cad5cd0f81ae1b9c810bd84a9663d0016b8719e7
rukhsarch <rukhsar@lucidspring.co>
2023-05-10 12:44:15 +0500

    end points updated

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import User from './models/User';
+import ShoppingCart from './models/Shopping';
 import authenticate from './middlewares/auth';
 
 const app = express();
@@ -45,15 +46,67 @@ app.post('/login', async (req, res) => {
   });
 });
 
-// 
-app.post('/shopping-lists', authenticate, async (req, res) => {
-  const userId = req.user._id;
-  // TODO: implement endpoint
+app.get('/shopping-lists/:userId', authenticate, async (req, res) => {
+  try {
+    // Check if the authenticated user is authorized to access this endpoint
+    if (req.user._id.toString() !== req.params.userId) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    // Find all shopping lists that have been shared with the user
+    const shoppingLists = await ShoppingCart.find({ "sharedWith.userId": req.params.userId });
+
+    return res.json(shoppingLists);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
-app.post('/share-shopping-lis', authenticate, async (req, res) => {
-  const userId = req.user._id;
-  // TODO: implement endpoint
+
+app.post('/share-shopping-list', authenticate, async (req, res) => {
+  try {
+    const { listId, sharedWith, permission } = req.body;
+
+    // Find the shopping List
+    const shoppingCart = await ShoppingCart.findById(listId);
+    if (!shoppingCart) {
+      return res.status(404).json({ message: 'Shopping cart not found' });
+    }
+
+    // Check if the authenticated user is the owner of the shopping cart
+    if (shoppingCart.userId !== req.userId) {
+      return res.status(403).json({ message: 'You are not the owner of this shopping cart' });
+    }
+
+    // Find the user to share the shopping cart with
+    const userToShareWith = await User.findOne({ email: sharedWith });
+    if (!userToShareWith) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Check if the shopping cart is already shared with the user
+    const existingShare = shoppingCart.sharedWith.find(
+      (share) => share.userId === userToShareWith._id.toString()
+    );
+    if (existingShare) {
+      return res.status(400).json({ message: 'Shopping cart is already shared with this user' });
+    }
+
+    // Add the user to the sharedWith array
+    shoppingCart.sharedWith.push({
+      userId: userToShareWith._id.toString(),
+      permission,
+    });
+
+    // Save the updated shopping cart
+    await shoppingCart.save();
+
+    res.status(200).json({ message: 'Shopping cart shared successfully' });
+  } catch (error) {
+    console.log('Error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 
